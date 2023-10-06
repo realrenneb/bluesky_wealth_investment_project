b97d67d188928849138c7ca19304bfabe29b3423
realrenneb <renneb@hotmail.co.uk>
2023-10-06 17:32:27 +0100

    files created from python, old files removed

--- Create_table_all_client_info.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE all_info AS
-SELECT *
-FROM fund_performance_data
-INNER JOIN fund_data USING (Fund_ID)
-INNER JOIN client_data USING (Fund_ID);
--- delete_bps_column.sql
+++
@@ -1,3 +0,0 @@
--- Delete duplicate columnn for bps from all_info'
-ALTER TABLE all_info
-DROP COLUMN Fund_BPS_Rate;
